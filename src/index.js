1e2c14967e8d0b516db8eb449cfbef813ec7395f
Cuong Duy Nguyen <103cuong@gmail.com>
2020-05-26 11:28:34 +0700

    🌳 Done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('lala');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import style from './style.css';
+
+const Index = () => {
+  return <div className={style.title}>Hello Webpack!</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById('index'));
